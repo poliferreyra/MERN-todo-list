3941933901c3d2c20edca9265f39ad3672fded8b
Poli Ferreyra <paoferreyra.fl@gmail.com>
2025-01-17 15:04:12 -0300

    Add Auth0 integration to backend; frontend implementation pending

--- src/app/backend/server.ts
+++ src/app/backend/server.ts
@@ -2,20 +2,36 @@ require("dotenv").config();
 
 import express from "express";
 import cors from "cors";
-// const cors = require('cors');
 
 const mongoose = require("mongoose");
 const todosRoutes = require("./routes/todos");
 
-
+const { auth, requiresAuth } = require("express-openid-connect");
 
 // express app
 const app = express();
 app.use(cors());
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.SECRET,
+};
+
 // middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" })); // Aquí configuramos CORS
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Permite solicitudes solo desde tu frontend
+    credentials: true, // Si usas cookies/sesiones en Auth0
+  })
+);
+
+app.use(auth(config));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -23,7 +39,10 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/todos", todosRoutes);
+app.use("/api/todos", requiresAuth(), todosRoutes);
+app.get("/", (req, res) => {
+  res.send((req as any).oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
 // connect to db
 mongoose
